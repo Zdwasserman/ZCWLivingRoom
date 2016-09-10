64461f36d2b8b0220d04bede6245a4e70b642305
Zachary Wasserman <Zdwasserman@gmail.com>
2016-09-10 15:54:37 -0400

    uploading test file

---
+++ src/main/java/Wasserman/Zachary/ZCWLivingRoom/Television.java
@@ -0,0 +1,21 @@
+package Wasserman.Zachary.ZCWLivingRoom;
+
+/**
+ * Created by zacharywasserman on 9/10/16.
+ */
+
+public class Television {
+    private Integer volume;
+    private Integer channel;
+    public Television(){
+        volume = 0;
+        channel = 0;
+    }
+    public Integer getVolume(){
+        return volume;
+    }
+    public void setVolume(Integer amount){
+        this.volume = volume;
+    }
+
+}
---
+++ src/test/java/Wasserman/Zachary/ZCWLivingRoom/TelevisionTest.java
@@ -0,0 +1,27 @@
+package Wasserman.Zachary.ZCWLivingRoom;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created by zacharywasserman on 9/10/16.
+ */
+
+public class TelevisionTest {
+    @Test
+    public void getVolumeTest(){
+        Television television = new Television();
+        Integer expectedValue = 0;
+        Integer actualValue = television.getVolume();
+        assertEquals("The value should be 0",expectedValue,actualValue);
+
+    }
+    @Test
+    public void setVolumeTest(){
+        Television television = new Television();
+        Integer expectedValue = 0;
+        Integer actualValue = television.getVolume();
+        assertEquals("The value should be null",expectedValue,actualValue);
+    }
+
+}
+
